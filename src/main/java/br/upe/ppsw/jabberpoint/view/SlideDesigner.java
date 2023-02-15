ae0323847a3401e800742cbd9e3cb11c8191b34b
rafa-souza-dev <rafiradeveloper@gmail.com>
2023-02-14 22:44:11 -0300

    criacao da classe SlideDesigner com responsabilidade unica

---
+++ src/main/java/br/upe/ppsw/jabberpoint/view/SlideDesigner.java
@@ -0,0 +1,43 @@
+package br.upe.ppsw.jabberpoint.view;
+
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.image.ImageObserver;
+
+import br.upe.ppsw.jabberpoint.model.Slide;
+import br.upe.ppsw.jabberpoint.model.SlideItem;
+import br.upe.ppsw.jabberpoint.model.Style;
+
+public class SlideDesigner {
+    private Slide slide;
+
+    public SlideDesigner(Slide slide) {
+        this.slide = slide;
+    }
+
+    public void execute(Graphics g, Rectangle area, ImageObserver view) {
+        float scale = this.getScale(area);
+    
+        int y = area.y;
+    
+        SlideItem slideItem = this.slide.getTitle();
+        Style style = Style.getStyle(slideItem.getLevel());
+        slideItem.draw(area.x, y, scale, g, style, view);
+    
+        y += slideItem.getBoundingBox(g, view, scale, style).height;
+    
+        for (int number = 0; number < this.slide.getSize(); number++) {
+          slideItem = (SlideItem) this.slide.getSlideItems().elementAt(number);
+    
+          style = Style.getStyle(slideItem.getLevel());
+          slideItem.draw(area.x, y, scale, g, style, view);
+    
+          y += slideItem.getBoundingBox(g, view, scale, style).height;
+        }
+    }
+
+    private float getScale(Rectangle area) {
+        return Math.min(((float) area.width) / ((float) Slide.WIDTH),
+            ((float) area.height) / ((float) Slide.HEIGHT));
+    }
+}
