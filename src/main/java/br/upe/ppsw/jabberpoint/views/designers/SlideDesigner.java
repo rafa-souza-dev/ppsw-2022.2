8a1257283fca18ae05b9774d9765e0c2b0e4f73a
rafa-souza-dev <rafiradeveloper@gmail.com>
2023-03-20 10:44:30 -0300

    implementando fabricas no algoritmo principal

--- src/main/java/br/upe/ppsw/jabberpoint/views/designers/SlideDesigner.java
+++ src/main/java/br/upe/ppsw/jabberpoint/views/designers/SlideDesigner.java
@@ -4,10 +4,10 @@ import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.awt.image.ImageObserver;
 
-import br.upe.ppsw.jabberpoint.models.BitmapItem;
 import br.upe.ppsw.jabberpoint.models.Slide;
 import br.upe.ppsw.jabberpoint.models.SlideItem;
-import br.upe.ppsw.jabberpoint.models.TextItem;
+import br.upe.ppsw.jabberpoint.views.designers.factories.MakeBoundingBoxGeneratorStrategy;
+import br.upe.ppsw.jabberpoint.views.designers.factories.MakeSlideItemDesignerStrategy;
 
 public class SlideDesigner {
     private Slide slide;
@@ -22,29 +22,24 @@ public class SlideDesigner {
         int y = area.y;
     
         SlideItem slideItem = this.slide.getTitle();
-        ISlideItemDesignerStrategy slideItemDesigner = null;
-        IBoundingBoxGeneratorStrategy boundingBoxGenerator = null;
+        MakeSlideItemDesignerStrategy makeSlideItemDesignerStrategy = new MakeSlideItemDesignerStrategy(slideItem);
+        MakeBoundingBoxGeneratorStrategy makeBoundingBoxGeneratorStrategy = new MakeBoundingBoxGeneratorStrategy(slideItem);
         ComponentsStyler style = ComponentsStyler.getStyle(slideItem.getLevel());
 
-        new TextItemDesignerStrategy().execute(slideItem, area.x, y, scale, g, style, view);
-        y += new TextItemBoundingBoxGeneratorStrategy().handle(slideItem, g, view, scale, style).height;
+        makeSlideItemDesignerStrategy.handle().execute(slideItem, area.x, y, scale, g, style, view);
+        y += makeBoundingBoxGeneratorStrategy.handle().handle(slideItem, g, view, scale, style).height;
         
         for (int number = 0; number < this.slide.getSize(); number++) {
           slideItem = (SlideItem) this.slide.getSlideItems().elementAt(number);
 
-          if (slideItem instanceof TextItem) {
-            slideItemDesigner = new TextItemDesignerStrategy();
-            boundingBoxGenerator = new TextItemBoundingBoxGeneratorStrategy();
-          } else if (slideItem instanceof BitmapItem) {
-            slideItemDesigner = new IBitmapItemDesignerStrategy();
-            boundingBoxGenerator = new BitmapBoundBoxGeneratorStrategy();
-          }
+          makeSlideItemDesignerStrategy.setSlideItem(slideItem);
+          makeBoundingBoxGeneratorStrategy.setSlideItem(slideItem);
 
           style = ComponentsStyler.getStyle(slideItem.getLevel());
 
-          slideItemDesigner.execute(slideItem, area.x, y, scale, g, style, view);
+          makeSlideItemDesignerStrategy.handle().execute(slideItem, area.x, y, scale, g, style, view);
     
-          y += boundingBoxGenerator.handle(slideItem, g, view, scale, style).height;
+          y += makeBoundingBoxGeneratorStrategy.handle().handle(slideItem, g, view, scale, style).height;
         }
     }
 
