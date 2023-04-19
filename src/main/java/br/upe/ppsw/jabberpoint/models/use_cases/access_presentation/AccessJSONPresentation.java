e7b1406a485d4dbfda0aab317b23dd23c0b2ab3d
rafa-souza-dev <rafiradeveloper@gmail.com>
2023-04-19 00:05:39 -0300

    implementado metodo de salvar arquivos em json

--- src/main/java/br/upe/ppsw/jabberpoint/models/use_cases/access_presentation/AccessJSONPresentation.java
+++ src/main/java/br/upe/ppsw/jabberpoint/models/use_cases/access_presentation/AccessJSONPresentation.java
@@ -2,10 +2,10 @@ package br.upe.ppsw.jabberpoint.models.use_cases.access_presentation;
 
 import java.io.IOException;
 import java.util.ArrayList;
-
-
+import java.util.Vector;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -18,7 +18,7 @@ import br.upe.ppsw.jabberpoint.models.Slide;
 import br.upe.ppsw.jabberpoint.models.SlideItem;
 import br.upe.ppsw.jabberpoint.models.TextItem;
 
-public class AccessJSONPresentation implements IBasicAcessor {
+public class AccessJSONPresentation implements IBasicAcessor, ISaveAcessor {
     @Override
     public void loadFile(Presentation presentation, String fileName) throws IOException {
         JSONObject jsonObject;
@@ -75,5 +75,50 @@ public class AccessJSONPresentation implements IBasicAcessor {
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
+    }
+
+    @Override
+    public void saveFile(Presentation presentation, String fileName) throws IOException {
+        JSONObject json = new JSONObject();
+        JSONArray slideJsonArray = new JSONArray();
+
+        json.put("title", presentation.getTitle());
+
+        for (int i = 0; i < presentation.getSize(); i++) {
+            Slide slide = presentation.getSlide(i);
+            Vector<SlideItem> items = slide.getSlideItems();
+
+            JSONObject jsonSlide = new JSONObject();
+            JSONArray jsonSlideItemArray = new JSONArray();
+            JSONObject jsonSlideItemObject = null;
+
+            for (int j = 0; j < items.size(); j++) {
+                SlideItem slideItem = items.get(j);
+                jsonSlideItemObject = new JSONObject();
+
+                jsonSlideItemObject.put("level", slideItem.getLevel());
+
+                if (slideItem instanceof TextItem) {
+                    jsonSlideItemObject.put("text", ((TextItem) slideItem).getText());
+                } else if (slideItem instanceof BitmapItem) {
+                    jsonSlideItemObject.put("imageName", ((BitmapItem) slideItem).getName());
+                }
+
+                jsonSlideItemArray.add(jsonSlideItemObject);
+            }
+
+            jsonSlide.put("title", slide.getTitleText());
+            jsonSlide.put("items", jsonSlideItemArray);
+
+            slideJsonArray.add(jsonSlide);
+        }
+
+        json.put("slides", slideJsonArray);
+
+        FileWriter writer = new FileWriter(fileName);
+
+        writer.write(json.toJSONString());
+
+        writer.close();
     }
 }
